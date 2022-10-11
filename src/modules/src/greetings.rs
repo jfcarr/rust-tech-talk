0f64d3bf109b6973742dcdb235b31c4655192eaf
Jim Carr <jfcarr@gmail.com>
2022-10-10 20:20:53 -0400

    Added modules example project.

---
+++ src/modules/src/greetings.rs
@@ -0,0 +1,7 @@
+pub fn say_hello() {
+    println!("Hello!");
+}
+
+pub fn say_goodbye() {
+    println!("Goodbye!");
+}
---
+++ src/modules/src/main.rs
@@ -0,0 +1,7 @@
+mod greetings;
+
+fn main() {
+    greetings::say_hello();
+
+    greetings::say_goodbye();
+}
